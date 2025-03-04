1a4177047b105610237483acfc12119125dbaa37
gotoxp45 <dhruvitgadhiya77@gmail.com>
2025-03-04 18:16:36 +0530

    Continue Watching API

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ import directorRouter from "./routes/director";
 import connectCloudinary from "./config/cloudinary";
 import seriesRouter from "./routes/series";
 import fileUpload from "express-fileupload";
+import continueWatchingRouter from "./routes/continueWatching";
 
 // Create Express server
 const app = express();
@@ -67,6 +68,7 @@ app.use("/watchlist", watchlistRouter);
 app.use("/search", searchRouter);
 app.use("/cast", castRouter);
 app.use("/director", directorRouter);
+app.use("/continue-watching", continueWatchingRouter);
 const port = PORT || 3000;
 
 Promise.all([connectDB(), connectCloudinary()])
--- src/routes/watchlist.ts
+++ src/routes/watchlist.ts
@@ -2,9 +2,9 @@ import express from "express";
 import { toggleWatchlist, getWatchlist } from "../controllers/watchlist";
 import { userAuth } from "../middlewares/Auth";
 
-const watchlistrouter = express.Router();
+const watchlistRouter = express.Router();
 
-watchlistrouter.post("/toggle", userAuth, toggleWatchlist);
-watchlistrouter.get("/", userAuth, getWatchlist);
+watchlistRouter.post("/toggle", userAuth, toggleWatchlist);
+watchlistRouter.get("/", userAuth, getWatchlist);
 
-export default watchlistrouter;
+export default watchlistRouter;
