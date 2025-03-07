d197401bdc7e107485ea9824cca221c7e9a264c1
Harsh9327 <harshmungra255@gmail.com>
2025-03-07 18:16:13 +0530

    minor changes

--- src/routes/review.ts
+++ src/routes/review.ts
@@ -9,7 +9,7 @@ import { userAuth } from "../middlewares/Auth";
 
 const reviewRouter = express.Router();
 
-reviewRouter.post("/addReview", userAuth, createOrUpdateReview); // Add and Update both in one
+reviewRouter.post("/addOrUpdateReview", userAuth, createOrUpdateReview); // Add and Update both in one
 reviewRouter.get("/getReview/:contentId", userAuth, getLatestReviews);
 reviewRouter.get("/movieWiseReview/:id", userAuth, getMovieWiseReview);
 reviewRouter.delete("/deleteReview/:reviewId", userAuth, deleteReview);
